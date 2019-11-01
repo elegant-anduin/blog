62e96c4752799d3c32fa853728a72ec1a25f2af3
elegant-anduin <ujovicic3@gmail.com>
2019-11-01 19:13:42 +0100

    okea

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,13 +16,6 @@ module.exports = {
     },
     {
       resolve: `gatsby-source-filesystem`,
-          options: {
-              path: `${__dirname}/static/assets`,
-              name: 'images',
-          },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
         path: `${__dirname}/src/`
